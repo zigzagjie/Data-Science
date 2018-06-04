e966a470ff1f5875932a5ef6ded64e2219b21adc
zigzagjie <31840384+zigzagjie@users.noreply.github.com>
2018-06-04 15:41:33 +0800

    Update String1.py

--- Pandas/String1.py
+++ Pandas/String1.py
@@ -7,3 +7,11 @@ However, you need to transform series to str function ready to apply string func
 # Task 1: Remove extra blanks
 s='hello the world        yes!'
 snew=" ".join(s.split())
+
+# Task 2: Find the index of nth occurence in a string
+def find_nth(haystack, needle, n):
+    start = haystack.find(needle)
+    while start >= 0 and n > 1:
+        start = haystack.find(needle, start+len(needle))
+        n -= 1
+    return start
