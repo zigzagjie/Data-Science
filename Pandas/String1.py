31df2402bc8630c612be4d1e88b45715b55c31f0
zigzagjie <31840384+zigzagjie@users.noreply.github.com>
2018-06-04 14:15:47 +0800

    Update String1.py

--- Pandas/String1.py
+++ Pandas/String1.py
@@ -1,1 +1,9 @@
-https://stackoverflow.com/questions/6531482/how-to-check-if-a-string-contains-an-element-from-a-list-in-python
+##https://stackoverflow.com/questions/6531482/how-to-check-if-a-string-contains-an-element-from-a-list-in-python
+
+"""
+In pandas dataframe, you usually need to apply function on string column. Typically they are just the same as the string function. 
+However, you need to transform series to str function ready to apply string function
+"""
+# Task 1: Remove extra blanks
+s='hello the world        yes!'
+snew=" ".join(s.split())
